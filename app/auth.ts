9394b65e587eafc29fa8f5a719a32c40efc81e82
iziuqo <iz.iuqo@gmail.com>
2024-12-04 11:52:56 -0300

    Update auth.ts

--- app/auth.ts
+++ app/auth.ts
@@ -11,11 +11,32 @@ export const {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     })
   ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub) {
+        session.user.id = token.sub
+      }
+      return session
+    },
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    }
+  },
   pages: {
     signOut: '/auth/signout'
-  },
-  debug: true
+  }
 })
