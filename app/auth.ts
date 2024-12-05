5fcafcc0f6e7ba3d8ae1acfbc87b09e4ba529fd3
iziuqo <iz.iuqo@gmail.com>
2024-12-05 16:44:37 -0300

    Update auth.ts

--- app/auth.ts
+++ app/auth.ts
@@ -1,9 +1,6 @@
 // app/auth.ts
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import GitHubProvider from "next-auth/providers/github"
-import AzureADProvider from "next-auth/providers/azure-ad"
-import Auth0Provider from "next-auth/providers/auth0"
 
 export const {
   handlers: { GET, POST },
@@ -22,20 +19,6 @@ export const {
           response_type: "code"
         }
       }
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-    AzureADProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID!,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
-      tenantId: process.env.AZURE_AD_TENANT_ID,
-    }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID!,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
-      issuer: process.env.AUTH0_ISSUER,
     })
   ],
   pages: {
