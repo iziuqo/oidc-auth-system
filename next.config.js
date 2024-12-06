eac5b252da83ec49cd833fc2e71a019acfb17b45
iziuqo <iz.iuqo@gmail.com>
2024-12-05 23:05:25 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '/a/**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  }
 }
 
 module.exports = nextConfig
